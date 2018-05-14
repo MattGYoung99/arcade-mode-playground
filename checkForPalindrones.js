7d8c76f00905d247d8f750b206a6eda65521f428
Matt Young <mattgyoung99@gmail.com>
2018-05-14 02:05:09 -0500

    codefights algorithms

---
+++ checkForPalindrones.js
@@ -0,0 +1,6 @@
+// INSTRUCTIONS
+//Given the string, check if it is a palindrome.
+
+function checkPalindrome(inputString) {
+	return (inputString == inputString.split('').reverse().join('')) ? true : false
+}
