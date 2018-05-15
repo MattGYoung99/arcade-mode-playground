f12bd759c12b9ee334d4275f746ec85f7b28b8fa
Matt Young <mattgyoung99@gmail.com>
2018-05-15 17:25:04 -0500

    max multiple and seatsInTheater challenges for codefights

---
+++ maxMultiple.js
@@ -0,0 +1,9 @@
+// Given a divisor and a bound, find the largest integer N such that:
+
+//     N is divisible by divisor.
+//     N is less than or equal to bound.
+//     N is greater than 0.
+
+// It is guaranteed that such a number exists.
+
+const maxMultiple = (divisor, bound) => bound - (bound % divisor)
